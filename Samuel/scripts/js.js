144e494213e9e1c5778e2994ff07290ba29146aa
CashmereBob <weidmar@gmail.com>
2016-05-18 16:31:14 +0200

    reciept klar

--- Samuel/scripts/js.js
+++ Samuel/scripts/js.js
@@ -51,3 +51,36 @@ function removeFromCart() {
     location.reload();
 
 }
+
+function kvitto() {
+    kassan();
+    var url = window.location.href.split("?");
+    var param = url[1].split("&");
+
+    var pair = [];
+    for (var i = 0, len = param.length; i < len; ++i) {
+        var parameter = param[i].split("=");
+
+        pair.push(parameter[1]);
+    }
+
+
+    var doc = document.getElementById("levadress");
+    doc.getElementsByTagName('p')[0].innerHTML = pair[3];
+    doc.getElementsByTagName('p')[1].innerHTML = pair[4];
+    doc.getElementsByTagName('p')[2].innerHTML = pair[5];
+    doc.getElementsByTagName('p')[3].innerHTML = pair[6];
+    doc.getElementsByTagName('p')[4].innerHTML = pair[7];
+
+    var doc2 = document.getElementById("faktadress");
+    doc2.getElementsByTagName('p')[5].innerHTML = pair[8];
+    doc2.getElementsByTagName('p')[6].innerHTML = pair[9];
+    doc2.getElementsByTagName('p')[7].innerHTML = pair[10];
+    doc2.getElementsByTagName('p')[8].innerHTML = pair[11];
+    doc2.getElementsByTagName('p')[9].innerHTML = pair[12];
+    doc2.getElementsByTagName('p')[10].innerHTML = pair[13];
+
+    removeFromCart();
+
+
+}
