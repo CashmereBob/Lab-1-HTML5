b2666e7c112d8788d6076cccb6b849f9ba1d10d4
CashmereBob <weidmar@gmail.com>
2016-05-18 13:08:31 +0200

    Fixar kassa i JS

---
+++ Samuel/scripts/js.js
@@ -0,0 +1,34 @@
+﻿function addToCart(id) {
+    var orderQuantity = document.getElementById(id).getElementsByTagName("input")[0].value;
+    var produktPrice = document.getElementById(id).getElementsByTagName("h3")[0].innerHTML;
+    var produktName = document.getElementById(id).getElementsByTagName("legend")[0].innerHTML;
+    var totalPrice = parseInt(produktPrice) * orderQuantity;
+    
+    var outputLine = orderQuantity + 'st ' + produktName + ' a´' + produktPrice + ' Total: ' + totalPrice;
+
+    sessionStorage.setItem(id, orderQuantity + ', ' + produktName + ', ' + produktPrice + ', ' + totalPrice);
+
+    return window.alert(orderQuantity + 'st ' + produktName + ' lagda i varukorgen');
+}
+
+function kassan() {
+
+    var kassa = document.getElementById("kundvagn").getElementsByTagName("table")[0];
+    var totalAntal = 0;
+    var totalPris = 0;
+
+    for (var i = 0, len = sessionStorage.length; i < len; ++i) {
+
+        var produktArray = sessionStorage.getItem(sessionStorage.key(i)).split(",");
+
+        totalAntal += produktArray[0];
+        totalPris += produktArray[3];
+
+        var node = document.createElement("tr");
+        node.innerHTML = '<td>' + produktArray[0] + '</td><td>' + produktArray[1] + '</td><td>' + parseInt(produktArray[2]) + '</td><td>' + produktArray[3] + '</td>';
+
+        kassa.appendChild(node)   
+    }
+
+}
+
